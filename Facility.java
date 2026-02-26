ca8d7cdfdabb270b3eec6063f91ed2e6fd4089bf
Amali-0 <amalabdi54321@gmail.com>
2026-02-26 13:26:52 +0300

    Add abstract Facility class with location property

---
+++ Facility.java
@@ -0,0 +1,9 @@
+public abstract class Facility {
+    private String location;
+
+    public Facility(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() { return location; }
+}
